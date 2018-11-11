6021e854a640b54ea0e125ed5748bff20184f89c
Wojciech Szućko <wojciech.szucko@gmail.com>
2018-11-11 07:36:47 +0100

    feat: handle operation CRUD

--- src/modules/budget/controllers/budget.controller.ts
+++ src/modules/budget/controllers/budget.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { BudgetService } from '../services/budget.service';
 import { DefaultResponse } from '../../../utilities/types/default-response';
 import { Budget } from '../../../utilities/types/budget';
+import { Operation } from '../../../utilities/types/operation';
 
 @Controller('budget')
 export class BudgetController {
@@ -12,6 +13,36 @@ export class BudgetController {
 	public getBudget(): DefaultResponse<Budget> {
 		return {
 			data: this.service.getBudget(),
+			error: null,
+		};
+	}
+
+	@Post('/operations')
+	public addOperation(@Body() body: Operation): DefaultResponse<Operation> {
+		const operation = this.service.addOperation(body);
+
+		return {
+			data: operation,
+			error: null,
+		};
+	}
+
+	@Put('/operations/:id')
+	public editOperation(@Param() params, @Body() body: Operation): DefaultResponse<boolean> {
+		this.service.editOperation(params.id, body);
+
+		return {
+			data: true,
+			error: null,
+		};
+	}
+
+	@Delete('/operations/:id')
+	public removeOperation(@Param() params): DefaultResponse<boolean> {
+		this.service.removeOperation(params.id);
+
+		return {
+			data: true,
 			error: null,
 		};
 	}
